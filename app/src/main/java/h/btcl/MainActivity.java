e5767aaf91f1b184f227417c6d4b01fc8ca93f6e
Ganesh <ganesh.g.cse@gmail.com>
2016-07-04 15:09:16 +0530

    Initial commit

---
+++ app/src/main/java/h/btcl/MainActivity.java
@@ -0,0 +1,13 @@
+package h.btcl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
